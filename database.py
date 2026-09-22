363b842ca4187c029c930f9c6b6dbed2f150687b
unknown <muktadirtaseen98@gmail.com>
2026-09-22 16:56:45 +0600

    final commit

--- database.py
+++ database.py
@@ -5,8 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres.hdareqyxtkfcieuibugm:chuditore100@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
